37f07704ab7673a7860ebc23b1802e614e83ef76
Juraj Tomko <xtomko@nymfe73.fi.muni.cz>
2015-09-23 10:33:56 +0200

    first commit

---
+++ src/main/java/cz/muni/fi/myproject/App.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.myproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
